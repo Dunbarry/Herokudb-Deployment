8b52821982244982276e003d9fd59794653f1217
Dunbarry <RyanADunbar@gmail.com>
2016-09-02 11:51:15 -0600

    um...

--- migrations/20160902105234_initial.js
+++ migrations/20160902105234_initial.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('taco_toppings', function (table){
+    table.increments();
+    table.string('name');
+    table.integer('price_in_cents')
+    table.timestamp('time');
+  })
+
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('taco_toppings');
 };
